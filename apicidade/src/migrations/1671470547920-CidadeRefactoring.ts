21bda4cba0d844be2da67730a46ef722d786c2b8
Ronny Pereira <ronny.pereira@ipdec.org>
2022-12-19 16:01:25 -0400

    problema na migração

--- apicidade/src/migrations/1671470547920-CidadeRefactoring.ts
+++ apicidade/src/migrations/1671470547920-CidadeRefactoring.ts
@@ -3,13 +3,13 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class CidadeRefactoring1671470547920 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "cidade" RENAME COLUMN "nome_cidade" TO "nome"`,
+      `ALTER TABLE "cidade" RENAME COLUMN "nome"RENAME TO "nome_cidade"`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "cidade" RENAME COLUMN "nome" TO "nome_cidade"`,
+      `ALTER TABLE "cidade" RENAME COLUMN "nome" RENAME TO "nome_cidade"`,
     );
   }
 }
