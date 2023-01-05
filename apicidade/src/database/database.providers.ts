0374d1afa9604237e9e9cd5de3f4dd886280f66d
Ronny Pereira <ronny.pereira@ipdec.org>
2023-01-05 08:53:09 -0400

    Problema para subir o dockercompose

--- apicidade/src/database/database.providers.ts
+++ apicidade/src/database/database.providers.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export const dataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
+  host: 'db',
   port: 5432,
   username: 'postgres',
   password: 'postgres',
-  database: 'postgres',
+  database: 'cidade-app',
   entities: ['dist/**/**/entities/*{.ts,.js}'],
   migrationsTableName: 'migrations',
   migrations: ['dist/migrations/*{.ts,.js}'],
--- apicidade/src/migrations/1671470547920-CidadeRefactoring.ts
+++ apicidade/src/migrations/1671470547920-CidadeRefactoring.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class CidadeRefactoring1671470547920 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "cidade" RENAME COLUMN "nome_cidade" TO "nome"`,
+      `ALTER TABLE "cidade" RENAME COLUMN "nome_cidade" TO "nome_cidade"`,
     );
   }
 
